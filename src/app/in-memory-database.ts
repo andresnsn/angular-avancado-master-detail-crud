708348f00d8b318a06ffc2a449de6899971c98e7
andresnsn <59550765+andresnsn@users.noreply.github.com>
2021-02-18 14:16:21 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,19 @@
+import { httpClientInMemBackendServiceFactory, InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
+import { Observable } from "rxjs";
+
+export class InMemoryDatabase implements InMemoryDbService {
+
+    createDb() {
+        const categories = [
+            { id: 1, name: "Moradia", description: "Pagamentos de Contas da Casa"},
+            { id: 2, name: "Saúde", description: "Plano de saúde e Remédios"},
+            { id: 3, name: 'Lazer', description: "Cinema, parques, praia, etc"},
+            { id: 4, name: "Salário", description: "Recebendo de Salário"},
+            { id: 5, name: "Freelas", description: "Trabalhos como freelancer"}
+        ];
+
+        return { categories }
+
+    }
+
+}
